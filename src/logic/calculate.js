94eb9e2081dd5727e892ab94e3dc48dc6eba5749
Kalib Hackin <nagui08@hotmail.com>
2019-12-18 17:45:51 -0500

    calculate file done

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,57 @@
-const calculate = ({ total, next, operation }, buttonName) => {
-  
-}
+import operate from './operate'
+
+const calculate = (obj, buttonName) => {
+  const objData = obj;
+  const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const OPERATIONS = ['%', '/', 'X', '-', '+'];
+  if (objData.total === 'Error') {
+    objData.total = null;
+  } else if (OPERATIONS.includes(buttonName)) {
+    if (objData.total && !objData.operation) {
+      objData.operation = buttonName;
+    } else if (objData.total && objData.operation && objData.next) {
+      objData.total = operate(objData.total, objData.next, objData.operation);
+      objData.next = null;
+      objData.operation = buttonName;
+    }
+  } else if (NUMBERS.includes(buttonName)) {
+    if (objData.total && objData.operation && !objData.next) {
+      objData.next = buttonName;
+    } else if (objData.total && objData.operation && objData.next) {
+      objData.next += buttonName;
+    } else if (objData.total != null) {
+      objData.total += buttonName;
+    } else {
+      objData.total = buttonName;
+    }
+  } else if (buttonName === 'AC') {
+    objData.total = null;
+    objData.next = null;
+    objData.operation = null;
+  } else if (buttonName === '+/-') {
+    if (objData.next != null) {
+      objData.next = objData.next * -1;
+    } else if (objData.total != null) {
+      objData.total = objData.total * -1;
+    }
+  } else if (buttonName === '.') {
+    if (objData.total != null && objData.operation != null && objData.next != null) {
+      if (!/\./.test(objData.next)) {
+        objData.next += buttonName;
+      }
+    } else if (objData.total != null && objData.operation === null && objData.next === null) {
+      if (!/\./.test(objData.total)) {
+        objData.total += buttonName;
+      }
+    }
+  } else if (buttonName === '=') {
+    if (objData.total != null && objData.operation != null && objData.next != null) {
+      objData.total = operate(objData.total, objData.next, objData.operation);
+      objData.next = null;
+      objData.operation = null;
+    }
+  }
+  return objData;
+};
+
+export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,13 +1,20 @@
-import Big from 'big';
+import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
   let n1 = Big(numberOne);
   let n2 = Big(numberTwo);
-  if (operation === '+') { return n1 + n2}
-  if (operation === '-') { return n1 - n2}
-  if (operation === 'X') { return n1 * n2}
-  if (operation === '/') { return n1 / n2}
-  if (operation === '%') { return n1 % n2}
+  if (operation === '+') { 
+    return n1 + n2
+  };
+  if (operation === '-') {
+    return n1 - n2
+  };
+  if (operation === 'X') {
+    return n1 * n2
+  };
+  if (operation === '/') {
+    return n1 / n2
+  };
 };
 
 export default operate;
