982e20162fbf1290f6a63d57a43d138e5234cf22
Kalib Hackin <nagui08@hotmail.com>
2019-12-17 20:25:46 -0500

    styling button panel

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({ name }) => (
-  <div><button type="button">{name}</button></div>
+  <button type="button">{name}</button>
 );
 
 Button.propTypes = {
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,31 +4,31 @@ import Button from './Button';
 
 const ButtonPanel = () => (
   <div>
-    <div>
+    <div className="row1">
       <Button name="AC" />
       <Button name="+/-" />
       <Button name="%" />
       <Button name="/" />
     </div>
-    <div>
+    <div className="row2">
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
       <Button name="X" />
     </div>
-    <div>
+    <div className="row3">
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
       <Button name="-" />
     </div>
-    <div>
+    <div className="row4">
       <Button name="1" />
       <Button name="2" />
       <Button name="3" />
       <Button name="+" />
     </div>
-    <div>
+    <div className="row5">
       <Button name="0" />
       <Button name="." />
       <Button name="=" />
