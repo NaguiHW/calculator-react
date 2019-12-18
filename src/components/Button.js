a6478fa6ed4960e3e8aa031efc95dd2e51be788a
Kalib Hackin <nagui08@hotmail.com>
2019-12-17 20:37:01 -0500

    adding props to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <button type="button">{name}</button>
+const Button = ({ name, color, wide }) => (
+  <button type="button" style={{ background: color }} className={ wide ? 'wide' : 'normal' }>
+    {name}
+  </button>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
 };
+
+Button.defaultProps = {
+  color: 'orange',
+  wide: false,
+}
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,32 +5,32 @@ import Button from './Button';
 const ButtonPanel = () => (
   <div>
     <div className="row1">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
+      <Button name="AC" color="lightgray" />
+      <Button name="+/-" color="lightgray" />
+      <Button name="%" color="lightgray" />
       <Button name="/" />
     </div>
     <div className="row2">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
+      <Button name="7" color="lightgray" />
+      <Button name="8" color="lightgray" />
+      <Button name="9" color="lightgray" />
       <Button name="X" />
     </div>
     <div className="row3">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
+      <Button name="4" color="lightgray" />
+      <Button name="5" color="lightgray" />
+      <Button name="6" color="lightgray" />
       <Button name="-" />
     </div>
     <div className="row4">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
+      <Button name="1" color="lightgray" />
+      <Button name="2" color="lightgray" />
+      <Button name="3" color="lightgray" />
       <Button name="+" />
     </div>
     <div className="row5">
-      <Button name="0" />
-      <Button name="." />
+      <Button name="0" color="lightgray" wide={true}/>
+      <Button name="." color="lightgray" />
       <Button name="=" />
     </div>
   </div>
