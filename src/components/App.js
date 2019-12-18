5eecfe15f0064af71aaf5be46768756fcefe9fb6
Kalib Hackin <nagui08@hotmail.com>
2019-12-18 18:22:38 -0500

    app file done

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,32 @@ import '../index.css';
 // eslint-disable-next-line
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <div className="container">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState(state => {
+      calculate(state, buttonName)
+    })
+  }
+
+  render() {
+    const toDisplay = this.state.next ? this.state.next : this.state.total;
+    return (
+      <div className="container">
+        <Display display={toDisplay}/>
+        <ButtonPanel clickHandler={this.handleClick}/>
+      </div>
+    )
+  }
+};
 
 export default App;
